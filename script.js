c0844c24f2c8544bffcd4e797a9159fe8b028558
Pranjal Kushwaha <132685212+KUpranjal@users.noreply.github.com>
2025-07-15 23:25:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name=name
+		this.age=age
+		
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	constructor(name,age,study){
+		super(name,age)
+		this.study=study
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	constructor(name , age,study,teach){
+		super(name,age,study)
+		this.teach=teach
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
