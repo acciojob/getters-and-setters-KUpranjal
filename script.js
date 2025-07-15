e704cd2e8e3822ed1dbe68ba8b6ea1e3bd193e7d
Pranjal Kushwaha <132685212+KUpranjal@users.noreply.github.com>
2025-07-15 23:26:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-//complete this code
 class Person {
-	constructor(name,age){
-		this.name=name
-		this.age=age
-		
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	greet() {
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 	}
 }
 
 class Student extends Person {
-	constructor(name,age,study){
-		super(name,age)
-		this.study=study
+	constructor(name, age, study) {
+		super(name, age);
+		this.study = study;
+	}
+
+	studyInfo() {
+		console.log(`${this.name} is studying ${this.study}.`);
 	}
 }
 
 class Teacher extends Person {
-	constructor(name , age,study,teach){
-		super(name,age,study)
-		this.teach=teach
+	constructor(name, age, teach) {
+		super(name, age);
+		this.teach = teach;
+	}
+
+	teachInfo() {
+		console.log(`${this.name} teaches ${this.teach}.`);
 	}
 }
 
